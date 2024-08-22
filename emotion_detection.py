cb76f3454b3f4d79766ff77850de2ba1aee8079c
almendoza25 <almendoza25@gmail.com>
2024-08-22 16:49:08 -0400

    Finished Task 2

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,8 @@
+import requests
+
 def emotion_detector(text_to_analyse):
-    URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    Input json: { "raw_document": { "text": text_to_analyse } }
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    text_obj = {"raw_document": {"text": text_to_analyse }}
+    response = requests.post(url, headers=header, json=text_obj)
+    return response.text
