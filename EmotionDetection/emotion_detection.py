f6117629a2c6d1e4cc9c347f654ff086b8fc8507
almendoza25 <almendoza25@gmail.com>
2024-08-31 17:12:46 -0400

    Finished Project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,14 +9,20 @@ def emotion_detector(text_to_analyse):
 
     formatted_response = json.loads(response.text)
 
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
-
-    emotion_dictionary = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
-
-    dominant_emotion = max(emotion_dictionary, key=emotion_dictionary.get)
+    if response.status_code == 200:
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        emotion_dictionary = {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
+        dominant_emotion = max(emotion_dictionary, key=emotion_dictionary.get)
+    elif response.status_code == 400:
+        anger = None
+        disgust = None 
+        fear = None
+        joy = None 
+        sadness = None 
+        dominant_emotion = None
     
     return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dominant_emotion} 
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
+''' This function initiates the Emotion Detection application
+    using Flask and deploying on localhost:5000
 '''
-
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -10,6 +8,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def send_emotion_text():
+    ''' This code receives text from user and runs 
+        emotion_detector() function. The output returns
+        the desired emotion score and the dominant emotion
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -21,10 +23,19 @@ def send_emotion_text():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, and 'sadness': {}. The dominant emotion is {}".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    # pylint: disable=consider-using-f-string
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {},"\
+    "'fear': {}, 'joy': {}, and 'sadness': {}. The dominant emotion is {}".format(anger, 
+    disgust, fear, joy, sadness, dominant_emotion)
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main
+        application page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
